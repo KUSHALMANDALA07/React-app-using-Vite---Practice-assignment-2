690afa6fa2d359c576edee342fffa6b726e9942f
KUSHALMANDALA07 <kushalmandala123@gmail.com>
2025-03-28 13:30:47 +0530

    react app

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1>Name: Mandala Kushal</h1>
+      <h2>Instagram ID: kushal_mandala07</h2>
+      <h2>E-mail id: mandala.kushal.s89@kalvium.community</h2>
+        
+    </>
+  )
+}
+
+export default App
